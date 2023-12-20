f99d0ebfe171103b7d0ebe0aeb1281a100b817ea
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2023-12-20 12:54:03 +0300

    configure template for data & proxies

--- config.rb
+++ config.rb
@@ -1,33 +1,20 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
-
 activate :importmap
 activate :directory_indexes
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
-
-# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+after_configuration do
+  proxy "index.html", "template.html", locals: { cemetery: nil }
+  @app.data.cemeteries.each do |cemetery|
+    proxy "#{cemetery}/index.html", "template.html", locals: { cemetery: cemetery }
+  end
+end
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
-
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
 
 # Helpers
 # Methods defined in the helpers block are available in templates
