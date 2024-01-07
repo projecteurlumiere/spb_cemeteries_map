466ad22b88819c9b55aaba59e3e40a295d094200
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-01-07 18:02:50 +0300

    add captions to photo gallery entries

--- source/javascripts/controllers/gallery_controller.js
+++ source/javascripts/controllers/gallery_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "stimulus"
 import PhotoSwipeLightbox from "photoswipe-lightbox";
 import PhotoSwipe from "photoswipe";
+import PhotoSwipeDynamicCaption from "photoswipe-dynamic-caption";
 
 export default class extends Controller {
 
@@ -9,6 +10,12 @@ export default class extends Controller {
       gallery: "#gallery",
       children: "a",
       pswpModule: () => PhotoSwipe
+    });
+
+
+    const captionPlugin = new PhotoSwipeDynamicCaption(lightbox, {
+      type: 'auto',
+      captionContent: '.pswp-caption-content'
     });
 
     lightbox.init();
