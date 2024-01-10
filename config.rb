2c8906eb65205ecc21dd20ff87a0eade0c16b5ec
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-01-10 19:12:16 +0300

    refactor gallery_pic partial(s)

--- config.rb
+++ config.rb
@@ -16,3 +16,29 @@ ready do
     proxy "#{cemetery}/index.html", "template.html", locals: { cemetery: cemetery }
   end
 end
+
+helpers do
+  def locals_for_gallery_pic(cemetery, photo, index)
+    link = "images/vendor/#{cemetery.path}/"
+    original_path = link + "#{photo.cid}_original.jpg"
+    basic_locals = {
+      photo:              photo,
+      original_path:      original_path,
+      custom_placeholder: nil,
+      custom_attributes:  nil
+    }
+
+    custom_locals = if index == 5
+      {
+        custom_placeholder: "Ещё #{cemetery.photos.count - 5} фото",
+        custom_attributes: { class: "gallery-n-more-link lightbox-opener" }
+      }
+    elsif index > 5
+      {
+      custom_placeholder: ""
+      }
+    end
+
+    custom_locals&.reverse_merge(basic_locals) || basic_locals
+  end
+end
