9f22397b7def631b73659f1830ed1c2d65cd0504
projecteurlumiere <129510705+projecteurlumiere@users.noreply.github.com>
2024-01-17 16:52:55 +0300

    implement catalogue search

--- source/javascripts/controllers/filter_controller.js
+++ source/javascripts/controllers/filter_controller.js
@@ -6,29 +6,50 @@ export default class extends Controller {
   }
 
   static targets = [
-    "button",
-    "initial" // button
+    "button"
   ]
 
+  search(event){
+    let searchInput = event.target.value.trim().toLowerCase();
+
+    Array.from(document.querySelectorAll(".preview")).forEach(preview => {
+      let previewText = preview.innerText.trim().toLowerCase();
+
+      if ((searchInput === "") || (previewText.includes(searchInput))) {
+        preview.classList.remove("hidden");
+      } else {
+        preview.classList.add("hidden");
+      }
+    })
+  }
+
+  // shows on the map and in the list the types which buttons have .selected class
   prepare() {
     this.buttonTargets.forEach(button => {
-      let layers = document.querySelectorAll(button.dataset.filterSelectorParam);
+      let selector = button.dataset.filterSelectorParam;
+      let layers = document.querySelectorAll(".layer" + selector);
+      let enabled;
 
       if (Array.from(button.classList).includes("select")) {
         this.filteredValue = true;
         layers.forEach(layer => {
           layer.classList.remove("hidden");
         })
+        enabled = true;
       } else {
         layers.forEach(layer => {
           layer.classList.add("hidden");
         })
+        enabled = false;
       }
+
+      this.#setFilteredValue();
+      this.#filterPreviews(enabled, selector);
     });
   }
 
   toggle(event) {
-    let layers = document.querySelectorAll(event.params.selector);
+    let layers = document.querySelectorAll(".layer" + event.params.selector);
     let enabled;
 
     layers.forEach(layer => {
@@ -42,35 +63,59 @@ export default class extends Controller {
     }
 
     this.#setFilteredValue();
+    this.#filterPreviews(enabled, event.params.selector)
   };
 
   toggleAll(){
     let layers = document.querySelectorAll(".layer");
+    let enabled;
 
     if (this.filteredValue === true) {
       layers.forEach(layer => {
-        layer.classList.remove("hidden");
+        layer.classList.add("hidden");
         this.filteredValue = false;
 
         this.buttonTargets.forEach(button => {
           button.classList.remove("select");
         });
       });
+      enabled = false
     } else {
       layers.forEach(layer => {
-        layer.classList.add("hidden");
+        layer.classList.remove("hidden");
         this.filteredValue = true;
 
         this.buttonTargets.forEach(button => {
           button.classList.add("select");
         });
       });
+      enabled = true
+    }
+
+    this.buttonTargets.forEach(button => {
+      let selector = button.dataset.filterSelectorParam
+      this.#filterPreviews(enabled, selector)
+    })
+  }
+
+  #filterPreviews(selected, selector) {
+    let previews = document.querySelectorAll(".preview" + selector)
+    debugger
+
+    if (selected) {
+      previews.forEach(preview => {
+        preview.classList.remove("hidden");
+      });
+    } else {
+      previews.forEach(preview => {
+        preview.classList.add("hidden");
+      });
     }
   }
 
   #setFilteredValue() {
     this.buttonTargets.forEach(button => {
-      if (button.classlist.includes("select")) {
+      if (Array.from(button.classList).includes("select")) {
         this.filteredValue = true;
         return
       }
